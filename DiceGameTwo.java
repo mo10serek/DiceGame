2d75d43a0b93a95d90062b7705fa385f95003379
mo10serek <michael.balcerzak@ocsbstudent.ca>
2016-09-07 16:39:20 -0400

    Add files via upload

---
+++ DiceGameTwo.java
@@ -0,0 +1,44 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class DiceGameTwo extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DiceGameTwo frame = new DiceGameTwo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public DiceGameTwo() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblJasDflkajs = new JLabel("jas df;lkajs");
+		contentPane.add(lblJasDflkajs, BorderLayout.CENTER);
+	}
+
+}
